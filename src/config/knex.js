2a5b53da7458818806cba5b4e4db9a3b005cc314
Renan Franca <renanfranca.dev@gmail.com>
2024-03-13 16:08:33 -0300

    chore: knex configures to conect with MySQL

--- src/config/knex.js
+++ src/config/knex.js
@@ -1,11 +1,10 @@
 module.exports = {
-    client: 'mysql2',
-    connection: {
-      host: 'testednc',
-      port: 3306,
-      user: 'user',
-      password: 'password',
-      database: 'testednc',
-    },
-  };
-  
+  client: "mysql2",
+  connection: {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "123456",
+    database: "test_jest",
+  },
+};
